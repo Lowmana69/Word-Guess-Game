19f78c93161860072bebb3188efd8ffafb0d3254
Lowmana69 <Lowmana69@gmail.com>
2019-06-15 16:03:24 -0700

    Added JavaScript variables and some HTML content

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+var taylorWords = ['HotDog', 'Western Digital', 'Quiet Time Clap'];
+var villians = ['Hannibal Lecter', 'Darth Vader', 'Dr. Evil'];
+var themeSongs = ['Harry Potter', 'James Bond', 'Haunted Mansion'];
+var tvShowsThemeSongs = ['Samuel L. Jackson', 'Leonidas', 'Chewbecca'];
+var celebrities = ['Beverly Hillbillies', 'Happy Days', 'Gilligan\'s Island'];
+
+var wins = 0;
+var loses = 0;
+var tries = 0;
+var guesses = ;
+
+
+
+var playersGuess = document.getElementById();
+var losesCounter = document.getElementById();
+var triesCounter = document.getElementById();
+var guessedCharacters = document.getElementById();
+var winsCounter = document.getElementById();
+
+var playersGuess = event.key;
+
+var playersGuess = document.getElementById().value;
+
+var randomization1 = taylorWords[Math.floor(Math.random() * taylorWords.length)];
+var randomization2 = villians[Math.floor(Math.random() * villianss.length)];
+var randomization3 = themeSongs[Math.floor(Math.random() * themeSongs.length)];
+var randomization4 = tvShowsThemeSongs[Math.floor(Math.random() * tvShowsThemeSongs.length)];
+var randomization5 = celebrities[Math.floor(Math.random() * celebrities.length)];
